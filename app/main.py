1a337340565e922b0a8d281edf371b35e32cf6f7
SaadMS10 <57102877+SaadMS10@users.noreply.github.com>
2020-12-28 19:00:45 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,18 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import webbrowser
-import time
-from pynput.mouse import Button, Controller
 from flask import Flask ,render_template  
 app = Flask(__name__) 
 @app.route('/')
 def index():
     webbrowser.open_new_tab("https://www.youtube.com/results?search_query=dashcam+russia")
-    time.sleep(2.2)
-    mouse = Controller()
-    mouse.position = (500, 350)
-    time.sleep(1.2)
-    mouse.click(Button.left,2)
     return render_template('base.html')
 
 
