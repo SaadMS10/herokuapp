fd7adf983e8198a221a104e815217e7a9e1e73d6
SaadMS10 <57102877+SaadMS10@users.noreply.github.com>
2020-12-28 19:03:00 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import webbrowser
+
 from flask import Flask ,render_template  
 app = Flask(__name__) 
 @app.route('/')
 def index():
     webbrowser.open_new_tab("https://www.youtube.com/results?search_query=dashcam+russia")
+   
     return render_template('base.html')
+
 
 
  
